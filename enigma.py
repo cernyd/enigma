2218241369df2ec7b16d35560efe48617aaf3b73
David Černý <cernyd@protonmail.com>
2016-10-15 12:23:11 +0200

    Building the enigma class.

---
+++ enigma.py
@@ -0,0 +1,10 @@
+class Enigma:
+    def __init__(self, rotor_order, plugboard_settings):
+        self.__rotor_order = rotor_order
+        self.__plugboard_settings = plugboard_settings
+
+    def button_press(self, button):
+        pass
+
+    def plugboard(self):
+        pass
