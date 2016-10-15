b55de5a0445a48accdea22985a4ab1032beeaa4d
David Černý <cernyd@protonmail.com>
2016-10-15 16:38:30 +0200

    The rotors will not contain rotation info on their own, the enigma class will manage that.

--- enigma.py
+++ enigma.py
@@ -1,10 +1,19 @@
+from rotor import Enigma1
+
+
 class Enigma:
-    def __init__(self, rotor_order, plugboard_settings):
+    def __init__(self, rotor_order, plugboard_settings=None):
         self.__rotor_order = rotor_order
         self.__plugboard_settings = plugboard_settings
+        print(self.__rotor_order)
 
     def button_press(self, button):
-        pass
+        input_letter = button
+
+        return input_letter
 
     def plugboard(self):
         pass
+
+
+my_enigma = Enigma([rotor for rotor in Enigma1.rotors.values()])
