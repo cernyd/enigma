ae65f5bd8bbfea009ead02e546d35badaee5f12e
David Černý <cernyd@protonmail.com>
2016-11-21 12:47:44 +0100

    Major improvements, have to fix the disabled radiobuttons.

--- rotor_gui.py
+++ rotor_gui.py
@@ -10,6 +10,8 @@ class RotorMenu(Toplevel):
     def __init__(self, curr_rotors, *args, **kwargs):
         Toplevel.__init__(self, *args, **kwargs)
 
+        self.curr_rotors = curr_rotors
+
         self.attributes("-alpha", 0.0)
         self.after(0, self.attributes, "-alpha", 1.0)
         # Load smoothness upgrade ^
@@ -29,8 +31,12 @@ class RotorMenu(Toplevel):
              Frame(self.root_frame, bd=1, relief='raised', bg='gray85'),
              Frame(self.root_frame, bd=1, relief='raised', bg='gray85')]
 
-        # Button
-        Button(button_frame, text='Apply', command=self.get_values,
+        # Buttons
+
+        Button(button_frame, text='Apply', command=self.destroy,
+               width=12, bg='gray85').pack(side='right', padx=5, pady=5)
+
+        Button(button_frame, text='Storno', command=self.storno,
                width=12, bg='gray85').pack(side='right', padx=5, pady=5)
 
         # Rotor stash
@@ -41,15 +47,15 @@ class RotorMenu(Toplevel):
         self.radio_groups = [[], [], [], []]
 
         # Rotors
-        rotors = ['I', 'II', 'III', 'IV', 'V', 'UKW-A', 'UKW-B', 'UKW-C']
-        rotors = [[val] * 2 for val in rotors]
+        rotors = ['I', 'II', 'III', 'IV', 'V' ]
+        reflectors = ['UKW-A', 'UKW-B', 'UKW-C']
 
         # Reflectors
         Label(self.rotor_frames[0], text='REFLECTOR', bg='gray85', bd=1,
               relief='sunken').pack(side='top', pady=5, padx=5)
-        for rotor, val in rotors[5:]:
-            radio = Radiobutton(self.rotor_frames[0], text=rotor,
-                                variable=self.rotor_vars[0], value=val,
+        for reflector in reflectors:
+            radio = Radiobutton(self.rotor_frames[0], text=reflector,
+                                variable=self.rotor_vars[0], value=reflector,
                                 bg='gray85')
             self.radio_groups[0].append(radio)
 
@@ -61,29 +67,44 @@ class RotorMenu(Toplevel):
                   bd=1,
                   relief='sunken').pack(side='top', pady=5, padx=5)
 
-            for rotor, val in rotors[:5]:
+            for rotor in rotors:
                 radio = Radiobutton(self.rotor_frames[index], text=rotor,
-                                    variable=self.rotor_vars[index], value=val,
+                                    variable=self.rotor_vars[index], value=rotor,
                                     bg='gray85')
                 self.radio_groups[index].append(radio)
             index += 1
 
-        index = 0
         for values in self.radio_groups:
             for radio in values:
                 radio.pack(side='top')
-                text = radio.config()['text'][4]
-                if text == curr_rotors[index]:
-                    radio.select()
-            index += 1
 
-        # [rotor.pack(side='top') for rotor in radios]
+        self.update_available()
 
         # Init
         [frame.pack(side='left', fill='both', padx=2) for frame in
          self.rotor_frames]
         button_frame.pack(side='bottom', fill='both')
         self.root_frame.pack(padx=10, pady=5)
+
+    def storno(self):
+        self.rotor_vars = []
+        self.destroy()
+
+    def update_available(self, event=None):  # Initial update
+        index = 0
+        for values in self.radio_groups:
+            for radio in values:
+                text = radio.config()['text'][4]
+                if text == self.curr_rotors[index]:
+                    radio.select()
+                elif text in self.curr_rotors:
+                    radio.config(state='disabled')
+                else:
+                    radio.config(state='active', bg='gray85')
+            index += 1
+
+    def update_selected(self, event=None):
+        pass
 
     def get_values(self, event=None):
         return [test.get() for test in self.rotor_vars]
