d8589878dffb66a8798ed2272eea70d1b5d3e14a
David Černý <cernyd@protonmail.com>
2016-11-25 16:45:24 +0100

    Steckerbrett ( or plugboard in english ) is finished on the frontend, will have to add logic behind it. This is the final phase of the gui. After this refactoring and debugging will follow.

--- plugboard_gui.py
+++ plugboard_gui.py
@@ -1,5 +1,14 @@
-from tkinter import Toplevel, Entry, Label, Frame
-from misc import Enigma1, get_icon
+from tkinter import Toplevel, Entry, Label, Frame, StringVar, Button
+from misc import get_icon, Enigma1
+from re import sub
+from string import ascii_uppercase
+
+
+layout = [[16,22,4,17,19,25,20,8,14],
+          [0,18,3,5,6,7,9,10],
+          [15,24,23,2,21,1,13,12,11]]
+
+labels = Enigma1.labels
 
 
 class PlugboardMenu(Toplevel):
@@ -7,7 +16,6 @@ class PlugboardMenu(Toplevel):
     def __init__(self, *args, **kwargs):
         Toplevel.__init__(self, *args, **kwargs)
 
-        self.config(bg='gray85')
         self.attributes("-alpha", 0.0)
         self.after(0, self.attributes, "-alpha", 1.0)
         # Load smoothness upgrade ^
@@ -18,21 +26,71 @@ class PlugboardMenu(Toplevel):
         # Window config
         self.grab_set()
         self.iconbitmap(get_icon('plugboard.ico'))
-        self.resizable(False, False)
+        #self.resizable(False, False)
         self.wm_title("Plugboard")
 
-        PlugboardMenu(self, 'test')
+        self.rows = []
+        self.plug_sockets = []
+
+        for row in layout:
+            new_row = Frame(self)
+            for item in row:
+                self.plug_sockets.append(PlugSocket(new_row, labels[item]))
+            self.rows.append(new_row)
+
+        for row in self.rows:
+            row.pack(side='top')
+
+        for item in self.plug_sockets:
+            item.pack(side='left')
+
+        button_frame = Frame(self)
+
+        self.apply_button = Button(button_frame, text='Apply',
+                                   command=self.destroy,
+                                   width=12)
+
+        self.storno_button = Button(button_frame, text='Storno',
+                                    command=self.storno,
+                                    width=12)
+
+        self.apply_button.pack(side='right', padx=5, pady=5)
+        self.storno_button.pack(side='right', padx=5, pady=5)
+
+        button_frame.pack(side='bottom', fill='x')
+
+    def storno(self):
+        self.plug_sockets = []
+        self.destroy()
 
 
-class PlugboardItem(Frame):
-    def __init__(self, label, *args, **kwargs):
-        Frame.__init__(self, *args, **kwargs)
+class PlugSocket(Frame):
+    """Custom made socket class"""
+    def __init__(self, parent, label):
+        Frame.__init__(self, parent)
 
-        Label(self, label=label).pack(side='top')
+        self.label = label
 
-        self.plug_socket = Entry(self)
+        input_trace = StringVar()
+        input_trace.set('')
 
-        self.plug_socket.pack(side='bottom')
+        Label(self, text=label).pack(side='top')
+
+        self.plug_socket = Entry(self, width=2, justify='center', textvariable=input_trace)
+
+        input_trace.trace('w', self.update_socket)
+
+        self.plug_socket.pack(side='bottom', pady=5)
 
     def get_socket(self):
+        """Gets string value of the socket"""
         return self.plug_socket.get()
+
+    def update_socket(self, *args, character=''):
+        """Ensures only a single, uppercase letter is entered"""
+        raw = character if character else self.plug_socket.get()
+        if raw:
+            self.plug_socket.delete('0', 'end')
+            string = sub(r"[^A-Za-z]", '', raw)[0].upper()
+            if string:
+                self.plug_socket.insert('0', string)
