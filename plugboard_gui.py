cbd7ed039f8edaf2a502ce12b9906f193770cf1b
cernyd <davidos313@gmail.com>
2016-11-25 14:53:15 +0100

    Minor update.

--- plugboard_gui.py
+++ plugboard_gui.py
@@ -1,4 +1,4 @@
-from tkinter import Toplevel, Entry, Label
+from tkinter import Toplevel, Entry, Label, Frame
 from misc import Enigma1, get_icon
 
 
@@ -20,3 +20,19 @@ class PlugboardMenu(Toplevel):
         self.iconbitmap(get_icon('plugboard.ico'))
         self.resizable(False, False)
         self.wm_title("Plugboard")
+
+        PlugboardMenu(self, 'test')
+
+
+class PlugboardItem(Frame):
+    def __init__(self, label, *args, **kwargs):
+        Frame.__init__(self, *args, **kwargs)
+
+        Label(self, label=label).pack(side='top')
+
+        self.plug_socket = Entry(self)
+
+        self.plug_socket.pack(side='bottom')
+
+    def get_socket(self):
+        return self.plug_socket.get()
