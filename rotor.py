f444d453ec62f73d462d433699147f42bbb76bba
David Černý <cernyd@protonmail.com>
2016-12-14 22:56:53 +0100

    Making everything more and more abstract, inheritance is lot more common now.

--- rotor.py
+++ rotor.py
@@ -1,35 +1,11 @@
 from misc import get_label, alphabet
 
 
-class Rotor:
-    """Class simulating (fairly) accurate enigma machine rotor behavior"""
-    def __init__(self, wiring='', turnover=0, position=0, setting=0, config_data=None):
-        """
-        :param wiring: Defines how letters are wired back and forth
-        :param turnover: Sets on which indicator letter the next rotor is turned
-        :param position: Rotor position relative to input
-        :param setting: Label indicator relative to internal wiring
-        """
-        self.__ring_setting = setting
-        self.__position = position
-        self.__front_board = alphabet
-        self.__back_board = wiring
-        self.turnover = turnover
-
-        if config_data:
-            self.load_config(config_data)
-        else:
-            self.position = position
-            self.ring_setting = setting
-
-        self.last_position = 0
-
-    # Label for the rotor menu
-
-    def get_label(self):
-        return get_label(self.__back_board)
-
-    # Routing functions
+class RotorBase:
+    def __init__(self, wiring=''):
+        self.front_board = alphabet
+        self.back_board = wiring
+        self.label = get_label(wiring)
 
     def forward(self, letter: str) -> str:
         """
@@ -37,7 +13,7 @@ class Rotor:
         :param letter: Input letter
         :return: Letter routed to the front
         """
-        return self.__back_board[alphabet.index(letter)]
+        return self.back_board[alphabet.index(letter)]
 
     def backward(self, letter: str) -> str:
         """
@@ -45,7 +21,46 @@ class Rotor:
         :param letter: Input letter
         :return:  Letter routed to the front
         """
-        return alphabet[self.__back_board.index(letter)]
+        return alphabet[self.back_board.index(letter)]
+
+    def dump_config(self):
+        """Dumps rotor configuration data"""
+        return dict(wiring=self.back_board)
+        # return dict(wiring=self.__back_board,
+        #             ring_setting=self.__ring_setting,
+        #             position=self.__position,
+        #             turnover=self.turnover)
+
+    def load_config(self, data):
+        """Loads rotor configuration data"""
+        self.back_board = data['wiring']
+        # self.__back_board = data['wiring']
+        # self.turnover = data['turnover']
+        # self.ring_setting = data['ring_setting']
+        # self.position = data['position']
+
+
+class Reflector(RotorBase):
+    pass
+
+
+class Rotor(RotorBase):
+    def __init__(self, wiring='', turnover=0, position=0, setting=0, config_data=None):
+        """
+        :param wiring: Defines how letters are wired back and forth
+        :param turnover: Sets on which indicator letter the next rotor is turned
+        :param position: Rotor position relative to input
+        :param setting: Label indicator relative to internal wiring
+        """
+        RotorBase.__init__(self, wiring)
+
+        self.turnover = turnover
+        self.position = position
+        self.setting = setting
+        self.last_position = 0
+
+        if config_data:
+            pass
 
     def rotate(self, places: int = 1) -> bool:
         """
@@ -54,21 +69,21 @@ class Rotor:
         :return: Info if the next position should be turned over
         """
         assert (places in range(-25, 26)), 'Can\'t rotate by "%d" places...' % places
-        self.last_position = self.__position
-        self.__position += places
+        self.last_position = self.position
+        self.position += places
 
         return_val = False
-        if self.__position == 26:
-            self.__position = 0
+        if self.position == 26:
+            self.position = 0
 
-        elif self.__position == -1:
-            self.__position = 25
+        elif self.position == -1:
+            self.position = 25
 
-        if self.__position == self.turnover:
+        if self.position == self.turnover:
             return_val = True  # Used to indicate if the next rotor should be moved
 
-        self.__front_board = self.__front_board[places:] + self.__front_board[:places]
-        self.__back_board = self.__back_board[places:] + self.__back_board[:places]
+        self.front_board = self.front_board[places:] + self.front_board[:places]
+        self.back_board = self.back_board[places:] + self.back_board[:places]
 
         return return_val
 
@@ -80,7 +95,7 @@ class Rotor:
         Property returning the private ring setting
         :return: Ring offset setting
         """
-        return self.__ring_setting
+        return self.ring_setting
 
     @ring_setting.setter
     def ring_setting(self, setting: int):
@@ -89,9 +104,9 @@ class Rotor:
         :param setting: Wiring offset relative to the indicator letters
         """
         assert (setting in range(0, 25)), 'Invalid ring setting "%s"...' % str(setting)
-        setting -= self.__ring_setting
-        self.__back_board = self.__back_board = self.__back_board[setting:] + self.__back_board[:setting]
-        self.__ring_setting = setting
+        setting -= self.ring_setting
+        self.back_board = self.back_board = self.back_board[setting:] + self.back_board[:setting]
+        self.ring_setting = setting
 
     # Position property
 
@@ -101,7 +116,7 @@ class Rotor:
         Property returning the private rotor position
         :return: Current position
         """
-        return self.__position
+        return self.position
 
     @position.setter
     def position(self, position: int):
@@ -111,20 +126,6 @@ class Rotor:
         """
         assert(position in range(0, 26)), 'Invalid position "%d"...' % position
 
-        position -= self.__position
+        position -= self.position
         self.rotate(position)
-        self.__position = position
-
-    def dump_config(self):
-        """Dumps rotor configuration data"""
-        return dict(wiring=self.__back_board,
-                    ring_setting=self.__ring_setting,
-                    position=self.__position,
-                    turnover=self.turnover)
-
-    def load_config(self, data):
-        """Loads rotor configuration data"""
-        self.__back_board = data['wiring']
-        self.turnover = data['turnover']
-        self.ring_setting = data['ring_setting']
-        self.position = data['position']
+        self.position = position
--- slot.py
+++ slot.py
@@ -3,52 +3,18 @@ from misc import Enigma1
 
 ring_labels = Enigma1.labels
 
-class Slot(Frame):
-    """Customizable slot frame for setting rotor and ring settings"""
-    def __init__(self, tk_master, master, index=0, kind='rotor', *args, **kwargs):
-        # Superclass constructor call
+
+class BaseSlot(Frame):
+    def __init__(self, master, text, tk_master=None, *args, **kwargs):
+        tk_master = tk_master if tk_master else master
         Frame.__init__(self, tk_master, bd=1, relief='raised', *args, **kwargs)
-
-        # Config stuff
-        labels = ('THIRD', 'SECOND', 'FIRST')
-        text = ''
-
-        if kind == 'rotor':
-            text = labels[index] + ' ROTOR'
-        elif kind == 'reflector':
-            text = 'REFLECTOR'
 
         Label(self, text=text, bd=1, relief='sunken').pack(side='top', padx=5,
                                                            pady=5)
 
-        # Master for editing enigma buffer and refreshing other slots
         self.master = master
-        self.index = index
-        self.kind = kind
         self.choice_var = StringVar()
         self.radio_group = []
-
-        items = []
-        if kind == 'rotor':
-            items.extend(list(Enigma1.rotors.keys()))
-            self.generate_contents(items)
-
-            # Ring setting indicator
-            self.ring_var = StringVar()
-
-            Label(self, text='RING\nSETTING', bd=1, relief='sunken').pack(side='top', fill='x', padx=4)
-            OptionMenu(self, self.ring_var, *ring_labels).pack(side='top')
-
-            setting_idx = self.master.enigma.rotors[index].ring_setting
-            curr_setting = ring_labels[setting_idx]
-            self.ring_var.set(curr_setting)
-
-            self.choice_var.set(self.master.enigma.rotors[index].get_label())
-            self.ring_var.trace('w', self.update_selected)
-        elif kind == 'reflector':
-            items.extend(list(Enigma1.reflectors.keys()))
-            self.generate_contents(items)
-            self.choice_var.set(self.master.enigma.reflector.get_label())
 
         self.choice_var.trace('w', self.update_selected)
         self.update_selected()
@@ -67,31 +33,67 @@ class Slot(Frame):
         Updates the master buffer
         :param event: Callback event handling
         """
-        if self.kind == 'reflector':
-            self.master.curr_reflector = self.choice_var.get()
-        elif self.kind == 'rotor':
-            self.master.curr_rotors[self.index] = self.choice_var.get()
-
-            ring_setting = Enigma1.labels.index(self.ring_var.get())
-            self.master.curr_ring_settings[self.index] = ring_setting
-
         self.master.update_all()
 
-    def update_available(self, event=None):
-        """
-        Updates all available rotors ( in slots )
-        :param event: Callback event handling
-        """
+    def update_available(self, type, event=None):
         for radio in self.radio_group:
-            if self.kind == 'reflector':
-                if radio['value'] == self.master.curr_reflector:
-                    if radio['value'] != self.choice_var.get():
-                        radio.config(state='disabled')
-                else:
-                    radio.config(state='active')
-            elif self.kind == 'rotor':
-                if radio['value'] in self.master.curr_rotors:
-                    if radio['value'] != self.choice_var.get():
-                        radio.config(state='disabled')
-                else:
-                    radio.config(state='active')
+            if radio['value'] == type:
+                if radio['value'] != self.choice_var.get():
+                    radio.config(state='disabled')
+            else:
+                radio.config(state='active')
+
+
+class RotorSlot(BaseSlot):
+    def __init__(self, master, index, tk_master=None, *args, **kwargs):
+        text = ('THIRD', 'SECOND', 'FIRST')[index] + ' ROTOR'
+
+        BaseSlot.__init__(self, master, text, tk_master, *args, **kwargs)
+
+        self.index = index
+
+        items = []
+        items.extend(list(Enigma1.rotors.keys()))
+        self.generate_contents(items)
+
+        # Ring setting indicator
+        self.ring_var = StringVar()
+
+        Label(self, text='RING\nSETTING', bd=1, relief='sunken').pack(
+            side='top', fill='x', padx=4)
+        OptionMenu(self, self.ring_var, *ring_labels).pack(side='top')
+
+        setting_idx = self.master.enigma.rotors[index].ring_setting
+        curr_setting = ring_labels[setting_idx]
+        self.ring_var.set(curr_setting)
+
+        self.choice_var.set(self.master.enigma.rotors[index].get_label())
+        self.ring_var.trace('w', self.update_selected)
+
+    def update_selected(self, event=None):
+        self.master.curr_rotors[self.index] = self.choice_var.get()
+        ring_setting = Enigma1.labels.index(self.ring_var.get())
+        self.master.curr_ring_settings[self.index] = ring_setting
+
+        BaseSlot.update_selected(self)
+
+    def update_available(self, event=None):
+        BaseSlot.update_available(type=self.master.curr_rotors)
+
+
+class ReflectorSlot(BaseSlot):
+    def __init__(self, master, tk_master=None, *args, **kwargs):
+        BaseSlot.__init__(self, master, 'REFLECTOR', tk_master, *args, **kwargs)
+
+        items = []
+        items.extend(list(Enigma1.reflectors.keys()))
+        self.generate_contents(items)
+        self.choice_var.set(self.master.enigma.reflector.get_label())
+
+    def update_selected(self, event=None):
+        self.master.curr_reflector = self.choice_var.get()
+
+        BaseSlot.update_selected(self)
+
+    def update_available(self, event=None):
+        BaseSlot.update_available(type=self.master.curr_reflector)
