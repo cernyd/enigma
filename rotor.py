b7f609a33d740c64ea0e5bdb9be681f95e0ec270
David Černý <cernyd@protonmail.com>
2016-12-17 15:13:12 +0100

    Refactoring, this time for real.

--- rotor.py
+++ rotor.py
@@ -11,19 +11,9 @@ class RotorBase:
         self.label = get_label(wiring)
 
     def forward(self, letter: str) -> str:
-        """
-        Routes a letter from front side to the back side
-        :param letter: Input letter
-        :return: Letter routed to the front
-        """
         return self.back_board[alphabet.index(letter)]
 
     def backward(self, letter: str) -> str:
-        """
-        Routes a letter from the back side to the front side
-        :param letter: Input letter
-        :return:  Letter routed to the front
-        """
         return alphabet[self.back_board.index(letter)]
 
     def dump_config(self):
@@ -44,12 +34,6 @@ class Reflector(RotorBase):
 
 class Rotor(RotorBase):
     def __init__(self, wiring='', turnover=0, position=0, setting=0, config_data=None):
-        """
-        :param wiring: Defines how letters are wired back and forth
-        :param turnover: Sets on which indicator letter the next rotor is turned
-        :param position: Rotor position relative to input
-        :param setting: Label indicator relative to internal wiring
-        """
         RotorBase.__init__(self, wiring)
 
         self.turnover = turnover
@@ -61,11 +45,6 @@ class Rotor(RotorBase):
             pass
 
     def rotate(self, places: int = 1) -> bool:
-        """
-        Rotates rotor by n places ( in any direction )
-        :param places: By how many places to rotate ( negative rotates back )
-        :return: Info if the next position should be turned over
-        """
         assert (places in range(-25, 26)), 'Can\'t rotate by "%d" places...' % places
         self.last_position = self.position
         self.position += places
@@ -85,25 +64,13 @@ class Rotor(RotorBase):
 
         return return_val
 
-    # Ring setting property
-
     def set_ring_setting(self, setting: int):
-        """
-        Sets ring setting ( wiring offset relative to the position indicator numbers )
-        :param setting: Wiring offset relative to the indicator letters
-        """
         assert (setting in range(0, 25)), 'Invalid ring setting "%s"...' % str(setting)
         setting -= self.ring_setting
         self.back_board = self.back_board = self.back_board[setting:] + self.back_board[:setting]
         self.ring_setting = setting
 
-    # Position property
-
     def set_position(self, position: int):
-        """
-        Sets rotor to the selected position instantly
-        :param position: Target position
-        """
         assert(position in range(0, 26)), 'Invalid position "%d"...' % position
 
         position -= self.position
