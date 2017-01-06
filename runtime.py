ef928e022a04e0bbf0afa94aa71a0e832903c023
David <davidos313@gmail.com>
2017-01-06 23:21:44 +0100

    Drastically reduced number of modules and dependencies by merging modules into groups.

--- runtime.py
+++ runtime.py
@@ -1,11 +1,16 @@
-from gui_components.root_gui import Root
 from unit_tests.unit_tests import TestEnigma, unittest
-from enigma_components.enigma import TkEnigma
+from enigma.enigma import TkEnigma
+from config_handler import Config
+
+
+# test = Config('config.xml')
+# print(test['globals.font'])
+
 
 # Main unittest before running, could warn about potential flaws
-# unittest.main(exit=False)
+unittest.main(exit=False)
 
-test = TkEnigma(object(),'UKW-C' ,['I', 'II', 'III'])
+# test = TkEnigma(object(),'UKW-C' ,['I', 'II', 'III'])
 # test.reflector = 'UKW-A'
 
 # if __name__ == '__main__':
