6fb556cffbde44627bf75cf124b0c7f068dfd9a3
David <davidos313@gmail.com>
2017-02-08 21:10:02 +0100

    Refactoring will be really hard, because I did not work on the project almost the whole month.

--- runtime.py
+++ runtime.py
@@ -1,25 +1,26 @@
-from gui import Root
-from enigma.components import *
+# from gui import Root
+from enigma.components import TestEnigma, unittest
+from cfg_handler import Config
 
 
-# config = Config('config.xml')
-# config.focus_buffer('globals')
+config = Config('config.xml')
+config.focus_buffer('globals')
 # config.get_data('font')
 # font = list(config.get_data('font').values())
 # bg = config.get_data('bg')['color']
-# enigma_cfg = config.get_data('enigma_defaults')
+enigma_cfg = config.get_data('enigma_defaults')
 # root = Root(enigma_cfg, Config('config.xml'), bg, font)
 #
 #
-# # Main unittest before running, could warn about potential flaws
-# if config.get_data(['unit_tests'])['startup_test'] == "True":
-#     TestEnigma.model = enigma_cfg['model']
-#     TestEnigma.cfg_path = ['config.xml']
-#     unittest.main(exit=False, verbosity=2)
+# Main unittest before running, could warn about potential flaws
+if config.get_data(['unit_tests'])['startup_test'] == "True":
+    TestEnigma.model = enigma_cfg['model']
+    TestEnigma.cfg_path = ['config.xml']
+    unittest.main(exit=False, verbosity=2)
 #
 #
 # if __name__ == '__main__':
 #     root.mainloop()
-
-factory = EnigmaFactory(['enigma', 'historical_data.xml'])
-print(factory.produce('EnigmaM3'))
+#
+# factory = EnigmaFactory(['enigma', 'historical_data.xml'])
+# print(factory.produce('EnigmaM3'))
