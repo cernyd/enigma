0143e5f3b6c81d8be1d724da4eee0b9bc7085777
David Černý <cernyd@protonmail.com>
2017-03-15 16:34:37 +0100

    Starting to build the global data handler.

--- runtime.py
+++ runtime.py
@@ -3,12 +3,7 @@ from enigma.components import TestEnigma, unittest, EnigmaFactory, EnigmaM4
 from cfg_handler import Config
 
 
-config = Config('config.xml')
-config.focus_buffer('globals')
-config.find('font')
-font = list(config.find('font').values())
-bg = config.find('bg')['color']
-enigma_cfg = config.find('enigma_defaults')
+
 root = Root(enigma_cfg, Config('config.xml'), bg, font)
 
 
