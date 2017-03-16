8e47d0e57185fbaf0fc06d28a3ce76d1f4f865e2
cernyd <davidos313@gmail.com>
2017-03-16 10:59:31 +0100

    Introducing data handler to everything ( not very nice but I want to know if it works ).

--- data_handler.py
+++ data_handler.py
@@ -1,11 +1,37 @@
 from cfg_handler import Config
+from enigma.components import EnigmaFactory
+from glob import glob
+from winsound import PlaySound, SND_ASYNC
+from os import path
+
+
+class Playback:
+    """Module for playing sounds from the sounds folder"""
+
+    def __init__(self, master_instance):
+        self.sounds = list(
+            map(lambda snd: snd[7:], glob(path.join('sounds', '*.wav'))))
+        self.master_instance = master_instance
+
+    def play(self, sound_name):
+        """Plays a sound based on the entered sound name"""
+        sound_name += '.wav'
+
+        if sound_name in self.sounds and self.master_instance.sound_enabled:
+            PlaySound(path.join('sounds', sound_name), SND_ASYNC)
 
 
 class DataHandler:
-    def __init__(self):
-        self.enigma = None
+    def __init__(self, master):
         self.global_cfg = Config('config.xml')
         self.global_cfg.focus_buffer('globals')
+        self.master = master
+        self.enigma_factory = EnigmaFactory(['enigma', 'historical_data.xml'])
+        self.enigma = self.enigma_factory.produce_enigma(**self.enigma_cfg, master=master)
+        self.playback = Playback(master)
+
+    def switch_enigma(self, model):
+        self.enigma = self.enigma_factory.produce_enigma(model, master=self)
 
     @property
     def font(self):
